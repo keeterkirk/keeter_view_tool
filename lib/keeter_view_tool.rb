2d4d08e4f805d75c00215a2707426c9e0297f418
Kirk Keeter <kirkkeeter@gmail.com>
2023-02-18 10:38:53 -0600

    Keeter tool initial commit

---
+++ lib/keeter_view_tool.rb
@@ -0,0 +1,7 @@
+require "keeter_view_tool/version"
+require "keeter_view_tool/renderer"
+
+module KeeterViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/keeter_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KeeterViewTool
+  VERSION = "0.1.0"
+end
